78cb7c89b815d965a26dc500aebe44e0e0e8c8ee
vietanh2102 <139680263+vietanh2102@users.noreply.github.com>
2024-04-16 13:57:39 +0700

    Update ProductContext.jsx

--- src/Context/ProductContext.jsx
+++ src/Context/ProductContext.jsx
@@ -10,7 +10,7 @@ function ProductProvider({ children }) {
     useEffect(() => {
         const getProduct = async () => {
             setLoading(true)
-            const res = await fetch(`https://hpvggl-8080.csb.app/products`);
+            const res = await fetch(`https://zrnvxy-8080.csb.app/products`);
             const data = await res.json();
             setProducts(data);
             setLoading(false)
